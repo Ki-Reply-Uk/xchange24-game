fcc3fe9166b416260b5deb591d08a2022db3c0d0
KiUKCodeFixAgent <152207718+KiUKCodeFixAgent@users.noreply.github.com>
2024-05-24 16:56:27 +0100

    File updated

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,7 +1,7 @@
 var Exchange = function() {
 
     let bugLeft = '1';                
-    let gameOver = false;
+    let gameOver = true;
     let userWon = false;
     let pauseTimer = false;
     
