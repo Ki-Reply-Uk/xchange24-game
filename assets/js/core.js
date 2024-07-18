1334b3ea38089f6d066f744f2330596f64b5abf6
Ki-Spaceship-Captain <kiukxchange24@gmail.com>
2024-07-18 11:29:54 +0100

    File updated

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,7 +1,7 @@
 var Exchange = function() {
 
     let bugLeft = '4';                
-    let gameOver = false;
+    let gameOver = true;
     let userWon = false;
     if (localStorage.getItem('pauseTimer') === null) {
         localStorage.setItem('pauseTimer', 'false');
